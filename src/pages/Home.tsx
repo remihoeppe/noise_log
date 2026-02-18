d52226f1bb95aaa686fa3d918030286f54876cc4
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2026-02-18 22:14:25 +0000

    Refactoring Home page to handle state of room selection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import Log from './Log';
 import roomsData from '../data/room';
+import { useState } from 'react';
 
 const Home = () => {
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const handleSelectedOption = (optionName: string) => {
+        setSelectedOption(optionName);
+    };
     return (
         <>
-            <Log optionsData={roomsData} />
+            <Log optionsData={roomsData} onSelected={handleSelectedOption} />
+            {selectedOption && <p>Selected option: {selectedOption}</p>}
         </>
     );
 };
--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -3,29 +3,27 @@ import RoomButton from '../components/buttons/RoomButton';
 
 const Log = ({
     optionsData,
+    onSelected,
 }: {
     optionsData: { id: number; name: string }[];
+    onSelected: (optionName: string) => void;
 }) => {
-    const [selectedOption, setSelectedOption] = useState<string | null>(null);
-
-    const handleSelectedOption = (option: string) => {
-        setSelectedOption(option);
+    const handleSelectedOption = (optionName: string) => {
+        onSelected(optionName);
     };
+
     return (
         <>
             <p>Select an option:</p>
-            {selectedOption && <p>Selected option: {selectedOption}</p>}
-            {!selectedOption && (
-                <div className="grid grid-cols-3 gap-4 p-4">
-                    {optionsData.map((option) => (
-                        <RoomButton
-                            key={option.id}
-                            onClick={() => handleSelectedOption(option.name)}>
-                            {option.name}
-                        </RoomButton>
-                    ))}
-                </div>
-            )}
+            <div className="grid grid-cols-3 gap-4 p-4">
+                {optionsData.map((option) => (
+                    <RoomButton
+                        key={option.id}
+                        onClick={() => handleSelectedOption(option.name)}>
+                        {option.name}
+                    </RoomButton>
+                ))}
+            </div>
         </>
     );
 };
