f0540b6a8e0e82bffff6c74526d69525b4bfadb7
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2026-02-18 22:35:48 +0000

    Adding ToggleButton option to Log page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import Log from './Log';
 import roomsData from '../data/room';
 import { useState } from 'react';
+import NoiseType from '../data/noiseType';
 
 const Home = () => {
     const [selectedRoom, setSelectedRoom] = useState<string | null>(null);
+    const [selectedNoiseType, setSelectedNoiseType] = useState<string | null>(
+        null,
+    );
+
+    const noiseTypeData = Object.values(NoiseType).map((noiseType) => ({
+        id: noiseType,
+        name: noiseType,
+    }));
+
     const handleSelectedOption = (
         optionName: string,
         setter: (optionName: string) => void,
@@ -12,6 +22,7 @@ const Home = () => {
     };
     return (
         <>
+            {/* Selection of current room */}
             {!selectedRoom && (
                 <Log
                     optionsData={roomsData}
@@ -21,6 +32,27 @@ const Home = () => {
                 />
             )}
             {selectedRoom && <p>Selected room: {selectedRoom}</p>}
+
+            {/* Selection of noise type */}
+            {!selectedNoiseType && (
+                <Log
+                    optionsData={noiseTypeData}
+                    onSelected={(optionName) =>
+                        handleSelectedOption(optionName, setSelectedNoiseType)
+                    }
+                />
+            )}
+            {selectedNoiseType && (
+                <p>Selected noise type: {selectedNoiseType}</p>
+            )}
+            {selectedNoiseType && (
+                <Log
+                    optionsData={noiseTypeData}
+                    onSelected={(optionName) =>
+                        handleSelectedOption(optionName, setSelectedNoiseType)
+                    }
+                />
+            )}
         </>
     );
 };
--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
 import RoomButton from '../components/buttons/RoomButton';
 
 const Log = ({
     optionsData,
     onSelected,
+    toggleable = false,
 }: {
-    optionsData: { id: number; name: string }[];
+    optionsData: { id: number | string; name: string }[];
     onSelected: (optionName: string) => void;
+    toggleable?: boolean;
 }) => {
     const handleSelectedOption = (optionName: string) => {
         onSelected(optionName);
@@ -16,13 +18,22 @@ const Log = ({
         <>
             <p>Select an option:</p>
             <div className="grid grid-cols-3 gap-4 p-4">
-                {optionsData.map((option) => (
-                    <RoomButton
-                        key={option.id}
-                        onClick={() => handleSelectedOption(option.name)}>
-                        {option.name}
-                    </RoomButton>
-                ))}
+                {optionsData.map((option) =>
+                    !toggleable ? (
+                        <RoomButton
+                            key={option.id}
+                            onClick={() => handleSelectedOption(option.name)}>
+                            {option.name}
+                        </RoomButton>
+                    ) : (
+                        <ToggleButton
+                            key={option.id}
+                            value={option.name}
+                            onClick={() => handleSelectedOption(option.name)}>
+                            {option.name}
+                        </ToggleButton>
+                    ),
+                )}
             </div>
         </>
     );
